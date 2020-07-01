ee7aa936a629931a6c7671f3735e3916438e55fa
Juan W. Zinser Sierra <zinserlopez@iMac-de-Juan.local>
2020-07-01 11:31:52 -0500

    cleans

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
-// Custom JavaScript For Public API Requests Project
-// Waiting for the document to be ready
-// refrences for important dom elements    
-// creating markup for search bar
-/* even though there's no need for interpolation, a template literal is best used here
-so the markup can be written without escape characters & maximize readability */
 
-
+// credit to
 //https://github.com/Jaydos/public-api-requests
 //https://github.com/MBoone5/Public-Api-Requests
 
@@ -16,16 +10,14 @@ const searchHTML = `
 </form>
 `;
 
-// appending the search HTML
 document.querySelector('.search-container').innerHTML = searchHTML;
 
-// function to create a card for each random user from an array of user objects
+
+// create cards html
 function cards(allUsers) {
-    // refrences for user information
     var gallery = document.getElementById('gallery');
     gallery.innerHTML = '';
 
-    // iterating over each random user object
     allUsers.forEach((user, i) => {
         // creating the HTML for the user card
         gallery.innerHTML += `
@@ -42,145 +34,116 @@ function cards(allUsers) {
         `;
     });
 
-
-// Add click listener to each profile card, display corresponding modal window for selected profile.
-let profiles = document.querySelectorAll('.card');
-profiles.forEach(profile => {
+// Displays model window when specific card is clicked.
+document.querySelectorAll('.card').forEach(profile => {
     profile.addEventListener('click', (e) => {
-        console.log("tro");
         displayModal(parseInt(e.currentTarget.id), allUsers);
     })
 })
+}
 
-} // close user card function
-// function to create a modal for each random user from an array of user objects
-// Function to create modal window for selected user.
+// function for displaying a card's modal window
 const displayModal = (profileIndex, data) => {
-var user = data[profileIndex];
-let modalContainer = document.createElement('div');
-modalContainer.className = 'modal-container';
-//modalContainer.style.backgroundColor = generateRandomColor();
-modalContainer.innerHTML = 
-    `<div class="modal">
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-            <img class="modal-img" src="${user.picture.large}" alt="profile picture">
-            <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
-            <p class="modal-text">${user.email}</p>
-            <p class="modal-text cap">${user.location.city}</p>
-            <hr>
-            <p class="modal-text">${user.cell}</p>
-            <p class="modal-text">${user.location.state}, ${user.location.postcode}</p>
-            <p class="modal-text">Birthday: ${user.dob.date.substring(0,10)}</p>
-    </div>
-    <div class="modal-btn-container">
-        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-        <button type="button" id="modal-next" class="modal-next btn">Next</button>
-    </div>`
+        var user = data[profileIndex];
+        let modalContainer = document.createElement('div');
+        modalContainer.className = 'modal-container';
+        modalContainer.innerHTML = 
+            `<div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${user.picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+                    <p class="modal-text">${user.email}</p>
+                    <p class="modal-text cap">${user.location.city}</p>
+                    <hr>
+                    <p class="modal-text">${user.cell}</p>
+                    <p class="modal-text">${user.location.state}, ${user.location.postcode}</p>
+                    <p class="modal-text">Birthday: ${user.dob.date.substring(0,10)}</p>
+            </div>
+            <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>`
 
-document.querySelector('body').appendChild(modalContainer);
+        document.querySelector('body').appendChild(modalContainer);
 
-// Remove modal window when 'X' button is clicked.
-document.querySelector('.modal-close-btn').addEventListener('click', () => {
-    document.querySelector('body').removeChild(document.querySelector('.modal-container'));
-})
+        // Remove modal window when 'X' button is clicked.
+        document.querySelector('.modal-close-btn').addEventListener('click', () => {
+            document.querySelector('body').removeChild(document.querySelector('.modal-container'));
+        })
 
-// Target 'Next' and 'Prev' buttons on modal window.
-const buttons = document.querySelectorAll('.modal-btn-container button');
+        // Target 'Next' and 'Prev' buttons on modal window.
+        const buttons = document.querySelectorAll('.modal-btn-container button');
 
-/*
-    Hide or display buttons depending on index of profile. 
-    Eg. If first profile is selected, 'Prev' button is hidden.
-    If last profile is selected, 'Next' button is hidden.
-    If only one profile is displayed, no buttons are visible.
-*/
-addOrRemoveButtons(profileIndex, data, buttons);
+        // hides (prev/next) buttons depending if the selected card is the first/last
+        addOrRemoveButtons(profileIndex, data.length, buttons);
 
-// Click listener for each button, displays either next or previous modal window.
-buttons.forEach(button => {
-    button.addEventListener('click', e => {
-        addOrRemoveButtons(profileIndex, data, buttons);
+        // Click listener for each button, displays either next or previous modal window.
+        buttons.forEach(button => {
+            button.addEventListener('click', e => {
+                addOrRemoveButtons(profileIndex, data.length, buttons);
 
-        document.querySelector('body').removeChild(modalContainer);
-        if(e.target.textContent === 'Next'){
-            displayModal(profileIndex + 1, data);
-        } else if (e.target.textContent === 'Prev'){
-            displayModal(profileIndex - 1, data);
-        }
-    })
-})
+                document.querySelector('body').removeChild(modalContainer);
+                if(e.target.textContent === 'Next'){
+                    displayModal(profileIndex + 1, data);
+                } else if (e.target.textContent === 'Prev'){
+                    displayModal(profileIndex - 1, data);
+                }
+            })
+        })
 }
 
 // Function to filter profiles depending on search bar value.
 const filterProfiles = (searchInput, data) => {
-// Empty existing filtered array.
-filteredProfiles = [];
-
-// If a modal window exists, remove it.
-if(document.querySelector('.modal-container')){
-    document.querySelector('body').removeChild('.modal-container');
+        filteredProfiles = [];
+        // If a modal window exists, remove it.
+        if(document.querySelector('.modal-container')){
+            document.querySelector('body').removeChild('.modal-container');
+        }
+        console.log(searchInput);
+        data.forEach(profile => {
+            console.log(profile.name.first);
+            if (profile.name.first.toLowerCase().includes(searchInput) || 
+                    profile.name.last.toLowerCase().includes(searchInput)){
+                filteredProfiles.push(profile);
+            }
+        })
+        displayOrRemoveErrorMessage(filteredProfiles);
+        console.log(filteredProfiles);
+        cards(filteredProfiles);
 }
-console.log(searchInput);
-// Filtering process, pushes matching profiles to filteredProfiles array.
-data.forEach(profile => {
-    console.log(profile.name.first);
-    if (profile.name.first.toLowerCase().includes(searchInput) || profile.name.last.toLowerCase().includes(searchInput)){
-        filteredProfiles.push(profile);
-    }
-})
 
-// Display or remove error message as required.
-displayOrRemoveErrorMessage(filteredProfiles);
-
-// Generate new profile cards using filteredProfiles array of objects.
-console.log(filteredProfiles);
-cards(filteredProfiles);
-}
 
 // Function to hide or show 'Next' or 'Prev' buttons depending on displayed profile.
-const addOrRemoveButtons = (displayedProfile, data, buttons) => {
-if(displayedProfile === 0 && data.length === 1){
-    buttons[0].style.visibility = 'hidden';
-    buttons[1].style.visibility = 'hidden';
-} else if (displayedProfile === 0){
-    buttons[0].style.visibility = 'hidden';
-    buttons[1].style.visibility = 'visible';
-} else if (displayedProfile === data.length -1){
+const addOrRemoveButtons = (displayedProfile, n_users, buttons) => {
     buttons[0].style.visibility = 'visible';
-    buttons[1].style.visibility = 'hidden';
-}
+    buttons[1].style.visibility = 'visible';
+
+    if(displayedProfile === 0 ){
+        buttons[0].style.visibility = 'hidden';
+    } 
+    if (displayedProfile === n_users -1){
+        buttons[1].style.visibility = 'hidden';
+    } 
 }
 
 // Function to check if no search matches are found. Displays error message if so, removes error message (if it exists) if not.
 const displayOrRemoveErrorMessage = results => {
-if(results.length === 0) {
-    if(document.querySelector('.errorMessage') === null){
-        let noResultsFound = document.createElement('p');
-        noResultsFound.className = 'errorMessage';
-        noResultsFound.textContent = 'No results found. Try again.';
-        document.querySelector('body').insertBefore(noResultsFound, gallery);
-    }      
-} else {
-    if(document.querySelector('.errorMessage') !== null){
-        document.querySelector('.errorMessage').style.display = 'none';
-    }       
-}
-}
-
-// Generate a random color to use as background color on modal windows.
-const generateRandomColor = () => {
-let numbers = [];
-
-while(numbers.length < 3){
-    numbers.push(Math.floor(Math.random() * (255 - 0 + 1) + 0));
+        if(results.length === 0) {
+            if(document.querySelector('.errorMessage') === null){
+                let noResults = document.createElement('p');
+                noResults.className = 'errorMessage';
+                noResults.textContent = 'No results found. Try again.';
+                document.querySelector('body').insertBefore(noResults, gallery);
+            }      
+        } else {
+            if(document.querySelector('.errorMessage') !== null){
+                document.querySelector('.errorMessage').style.display = 'none';
+            }       
+        }
 }
 
-let newColor = `rgba(${numbers[0]},${numbers[1]},${numbers[2]},0.5)`;
-return newColor;
-}
-
-
-// =========================== API REQUEST =========================================================
+// ------------------------------------ MAIN ------------------------------------------------
 // getting random user data
 fetch('https://randomuser.me/api/?results=12&inc=name,location,email,picture,cell,dob&nat=au&')
 .then(data => data.json())
@@ -188,7 +151,7 @@ fetch('https://randomuser.me/api/?results=12&inc=name,location,email,picture,cel
     // Generate a profile card for each result.
     cards(data.results);
     
-    // Submit event listener on form to filter profiles.
+    // search to filter profiles.
     document.getElementById('search-input').addEventListener('keyup', e => {
         e.preventDefault();
         filterProfiles(e.target.value.toLowerCase(), data.results);
--- js/script_jquery.js
+++ js/script_jquery.js
@@ -1,13 +1,9 @@
-// Custom JavaScript For Public API Requests Project
 // Waiting for the document to be ready
 $(document).ready(() => {
     // refrences for important dom elements
     const $galleryDiv = $('#gallery');
     const $modalDiv = $('#modal-div');
     
-    // creating markup for search bar
-    /* even though there's no need for interpolation, a template literal is best used here
-    so the markup can be written without escape characters & maximize readability */
     const searchHTML = `
     <form action="#" method="get">
         <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -15,15 +11,11 @@ $(document).ready(() => {
     </form>
     `;
 
-    // appending the search HTML
     $('.search-container').append(searchHTML);
 
-    // function to create a card for each random user from an array of user objects
     function cards(allUsers) {
-        // refrences for user information
         let cardTemplate = '';
 
-        // iterating over each random user object
         allUsers.forEach(user => {
             // creating the HTML for the user card
             cardTemplate += `
@@ -41,14 +33,12 @@ $(document).ready(() => {
         });
         
         return cardTemplate;
-    } // close user card function
-    // function to create a modal for each random user from an array of user objects
+    } 
     function modal(allUsers) {
         // creating the HTML for the user modals
         let modalTemplate = '';
         
         allUsers.forEach((user, index) => {
-            // references for user information
             modalTemplate += `
             <div class="modal-container">
                 <div class="modal">
@@ -83,34 +73,25 @@ $(document).ready(() => {
             const parents = $(e.target).parentsUntil('.gallery');
             let parentCard;
 
-            /* if there are no ancestors to traverse to .div, 
-            then e.target is the parent div.card
-            if there are ancestors, then the last element will be the parent div.card */
+
             if (parents.length === 0) {
                 parentCard = e.target;
             } else {
-                parentCard = parents[parents.length - 1]; // accessing the last element in the array
+                parentCard = parents[parents.length - 1]; 
             }
 
-            // finding the name of the employee
             const $empName = $(parentCard).find('.card-info-container>h3').text();
-
-            // reference to the current modal
             const $currentModal = $(`.modal-container:has(h3:contains(${$empName}))`);
-
-            // displaying the modal
             $currentModal.show();
-        }); // close display modal handler
-    } // close displayModal function
+        });
+    } 
     // function to handle the modal prev/next btns
-    function handleSlide() {
+    function addOrRemoveButtons() {
         // event listener for modal prev/next btns
         $('.modal-btn-container>button').click((e) => {
-            // finding the index of the current element
             const parentModal = $(e.target).parents('.modal-container')[0];
             let parentIndex = $('.modal-container').index(parentModal);
 
-            // finding the index of the target modal
             let targetIndex;
             if ($(e.target).attr('id') === 'modal-next') {
                 targetIndex = parentIndex + 1;
@@ -118,27 +99,25 @@ $(document).ready(() => {
                 targetIndex = parentIndex - 1;
             }
 
-            // hiding the current modal
             $(parentModal).hide();
-
-            // showing the target modal
             const targetModal = $('.modal-container').get(targetIndex);
             $(targetModal).show();
         });
     }
+
     // function to handle modal x button
-    function handleX() {
+    function closeModal() {
         $('.modal-close-btn').click((e) => {
             // find the parent modal and hide it
             let parentModal = $(e.target).parents('.modal-container')[0];
             $(parentModal).hide();
-        }); //
-    } // close handleX function
+        }); 
+    } 
+    
     // handling the search event
     function handleSearch() {
         // event listener on the search event
         $('#search-input').keyup((e) => {
-            // refrence to the input
             let inputVal = $(e.target).val().toLowerCase();
             
             // conditional to show or hide based on whether there is appropriate input to filter with
@@ -146,41 +125,30 @@ $(document).ready(() => {
                 // jqeury reference for the current .card
                 let $this = $(this); 
 
-                // if the current card contains the input: show the element, else: hide
                 if ($this.find(`h3:contains(${inputVal})`).length !== 0) {
                     $this.show();
                 } else {
                     $this.hide();
                 }
-            }); // close .card iteration
+            }); n
 
-        }); // close search event handler
-    } //close search event function
-    // =========================== API REQUEST =========================================================
-    // getting random user data
+        }); 
+    } 
 
+    // =========================== Main =========================================================
 
     $.getJSON('https://randomuser.me/api/?results=12&nat=us,gb&inc=picture,name,email,location,dob,phone', 
         data => {
             // Hiding the loader when the request is complete
             $('.loader').hide();
 
-            // appending the data into the gallery div
             $galleryDiv.append(cards(data.results));
 
-            // appending modals to the modal-div
             $modalDiv.append(modal(data.results));
-            
-            // handling modal display
             displayModal();
             $(".modal-container").hide();
-            // handling modal slide
-            handleSlide();
-
-            // handling the x functionality
-            handleX();
-
-            // handling the search function
+            addOrRemoveButtons();
+            closeModal();
             handleSearch();
 
         }); // close Random User API request    
